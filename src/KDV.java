2b554c06e7846cdd9cfb2e041037633ea10815a1
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-01-16 05:49:13 -0800

    KDV Project...

--- src/KDV.java
+++ src/KDV.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
+/*KDV Tutarı Hesaplayan Program
+Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+
+(Not : KDV tutarını 18% olarak alın)
+KDV'siz Fiyat = 10;
+KDV'li Fiyat = 11.8;
+KDV tutarı = 1.8;
+Ödev
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV
+tutarı hesaplayan programı yazınız.
+ */
 
 public class KDV {
     public static void main(String[] args) {
