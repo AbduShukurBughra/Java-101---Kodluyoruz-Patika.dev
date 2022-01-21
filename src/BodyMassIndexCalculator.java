b2d0670b2f12ae6891526d4d9c4b913e7c078aca
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-01-21 08:47:49 -0800

    Body Mass Index Calculator project

---
+++ src/BodyMassIndexCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/* Vücut Kitle İndeksi Hesaplama
+Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın. */
+
+public class BodyMassIndexCalculator {
+    public static void main(String[] args) {
+        double mass , boy, bodyMassIndex;
+
+        Scanner boyInput = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz : " );
+        boy=boyInput.nextDouble();
+        Scanner massInput = new Scanner(System.in);
+        System.out.println("Lütfen kilonuzu giriniz : ");
+        mass= massInput.nextDouble();
+        bodyMassIndex= mass / boy * boy;
+        System.out.println("Vücut Kitle İndeksiniz : " + bodyMassIndex );
+    }
+}
