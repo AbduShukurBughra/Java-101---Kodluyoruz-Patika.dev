a3085a3ab0c547d98cc43a249fadc3bc49470403
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-03 15:14:32 -0800

    User Login Project

---
+++ src/UserLogin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UserLogin {
+    public static void main(String[] args) {
+        String userName, password;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Kullanici Adiniz : ");
+        userName=input.nextLine();
+
+        System.out.println(" Sifreniz : ");
+        password=input.nextLine();
+
+        if(userName.equals("patika") && password.equals("java123")){
+            System.out.println("Giris Yaptiniz ! ");
+        }else{
+            System.out.println("Bilgileriniz Yanlis !");
+        }
+    }
+}
