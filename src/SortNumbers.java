d50799586d2cd86d6953e0a1cd14544c6559a59e
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-07 11:24:13 -0800

    Sort Number Project

--- src/SortNumbers.java
+++ src/SortNumbers.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 
 public class SortNumbers {
     public static void main(String[] args) {
-        int a,b,c;
+        int a, b, c;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("1. sayi : ");
-        a= input.nextInt();
+        System.out.println("1. sayi: ");
+        a = input.nextInt();
 
         System.out.println("2. sayi: ");
-        b= input.nextInt();
+        b = input.nextInt();
 
         System.out.println("3. sayi: ");
-        c=input.nextInt();
+        c = input.nextInt();
 
-        if((a>b)&&(a>c)){
-            if (b>c){
-                System.out.println("a>b>c");
-            }else if (c>b){
-
+        if ((a > b) && (a > c)) {
+            if ((b > c)) {
+                System.out.println("a > b > c");
+            } else {
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a> c");
+            } else {
+                System.out.println("b > c > a");
+            }
+        } else {
+            if (a > b) {
+                System.out.println("c > a > b");
+            } else {
+                System.out.println("c > b > a");
             }
         }
     }
