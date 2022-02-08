76840e3a6d8f178e8ae185059507a644fd1ac099
--Shukur <kurshadbughra@gmail.com>
2022-02-08 15:02:28 +0300

    Burcu Project

--- src/Burcu.java
+++ src/Burcu.java
@@ -3,156 +3,156 @@ import java.util.Scanner;
 public class Burcu {
     public static void main(String[] args) {
         int month, day;
-        String burcu="";
+        String burcu = "";
         boolean isError = false;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Dogdunuz ay : ");
-        month=input.nextInt();
+        month = input.nextInt();
 
         System.out.println("Dogdunuz gun : ");
-        day=input.nextInt();
+        day = input.nextInt();
 
-        switch(month){
+        switch (month) {
             case 1:
-                if(day>=1 && day<=31){
-                    if(day<22){
-                       burcu="Oglak";
-                    }else{
-                        burcu="Kova";
+                if (day >= 1 && day <= 31) {
+                    if (day < 22) {
+                        burcu = "Oglak";
+                    } else {
+                        burcu = "Kova";
                     }
-                }else{
+                } else {
                     isError = true;
                 }
                 break;
             case 2:
-                if(day>=1 && day<=28){
-                    if(day<20){
-                        burcu="Kova";
-                    }else{
-                        burcu="Balik";
+                if (day >= 1 && day <= 28) {
+                    if (day < 20) {
+                        burcu = "Kova";
+                    } else {
+                        burcu = "Balik";
                     }
-                }else{
+                } else {
                     isError = true;
                 }
                 break;
-            case 3 :
-                if (day>=1 && day<=31){
-                    if (day<20){
-                        burcu="Balik";
-                    }else{
-                        burcu="koc";
+            case 3:
+                if (day >= 1 && day <= 31) {
+                    if (day < 20) {
+                        burcu = "Balik";
+                    } else {
+                        burcu = "koc";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 4 :
-                if (day>=1 && day<=31){
-                    if (day<20){
-                        burcu="Koc";
-                    }else{
-                        burcu="Boga";
+            case 4:
+                if (day >= 1 && day <= 31) {
+                    if (day < 20) {
+                        burcu = "Koc";
+                    } else {
+                        burcu = "Boga";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 5 :
-                if (day>=1 && day<=31){
-                    if (day<22){
-                        burcu="Boga";
-                    }else{
-                        burcu="ikizler";
+            case 5:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22) {
+                        burcu = "Boga";
+                    } else {
+                        burcu = "ikizler";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 6 :
-                if (day>=1 && day<=31){
-                    if (day<23){
-                        burcu="ikizler";
-                    }else{
-                        burcu="Yengec";
+            case 6:
+                if (day >= 1 && day <= 31) {
+                    if (day < 23) {
+                        burcu = "ikizler";
+                    } else {
+                        burcu = "Yengec";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 7 :
-                if (day>=1 && day<=31){
-                    if (day<23){
-                        burcu="Yengec";
-                    }else{
-                        burcu="Aslan";
+            case 7:
+                if (day >= 1 && day <= 31) {
+                    if (day < 23) {
+                        burcu = "Yengec";
+                    } else {
+                        burcu = "Aslan";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 8 :
-                if (day>=1 && day<=31){
-                    if (day<23){
-                        burcu="Aslan";
-                    }else{
-                        burcu="Basak";
+            case 8:
+                if (day >= 1 && day <= 31) {
+                    if (day < 23) {
+                        burcu = "Aslan";
+                    } else {
+                        burcu = "Basak";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 9 :
-                if (day>=1 && day<=31){
-                    if (day<23){
-                        burcu="Basak";
-                    }else{
-                        burcu="Tersazi";
+            case 9:
+                if (day >= 1 && day <= 31) {
+                    if (day < 23) {
+                        burcu = "Basak";
+                    } else {
+                        burcu = "Tersazi";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 10 :
-                if (day>=1 && day<=31){
-                    if (day<23){
-                        burcu="Terazi";
-                    }else{
-                        burcu="Akrep";
+            case 10:
+                if (day >= 1 && day <= 31) {
+                    if (day < 23) {
+                        burcu = "Terazi";
+                    } else {
+                        burcu = "Akrep";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 11 :
-                if (day>=1 && day<=31){
-                    if (day<22){
-                        burcu="Akrep";
-                    }else{
-                        burcu="Yay";
+            case 11:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22) {
+                        burcu = "Akrep";
+                    } else {
+                        burcu = "Yay";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
-            case 12 :
-                if (day>=1 && day<=31){
-                    if (day<20){
-                        burcu="Yay";
-                    }else{
-                        burcu="Oglak";
+            case 12:
+                if (day >= 1 && day <= 31) {
+                    if (day < 20) {
+                        burcu = "Yay";
+                    } else {
+                        burcu = "Oglak";
                     }
-                }else{
-                    isError=true;
+                } else {
+                    isError = true;
                 }
                 break;
             default:
-                isError=true;
+                isError = true;
         }
-        if (isError){
+        if (isError) {
             System.out.println("Hatali giris yaptiniz: ");
-        }else{
-            System.out.println("Burcunuz : "+ burcu);
+        } else {
+            System.out.println("Burcunuz : " + burcu);
         }
     }
 }
