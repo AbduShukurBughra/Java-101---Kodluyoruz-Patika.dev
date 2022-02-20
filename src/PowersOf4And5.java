1aebe0e58c76b1212a35323b6a6ca710ac270587
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-19 23:50:06 -0800

    Powers Of 4 And 5  Project

---
+++ src/PowersOf4And5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PowersOf4And5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter a Number ");
+        int number = scanner.nextInt();
+
+        for (int i = 1, j = 1; i <= number && j <= number; i *= 4, j *= 5) {
+
+            if (i != 1 && j!=1) {
+                System.out.println(i);
+                System.out.println(j);
+            }
+        }
+    }
+}
