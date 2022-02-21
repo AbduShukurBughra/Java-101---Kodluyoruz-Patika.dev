b8b6d76b2504afb790542fc2ba463dfdf497899a
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-21 11:26:38 -0800

    Arm Strong Number Program Project

---
+++ src/SumOfTheDigitsOfArmStrongNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfTheDigitsOfArmStrongNumber {
+    public static void main(String[] args) {
+        int num;
+        int digit = 0,result = 0 ;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        num = input.nextInt();
+
+        while (num != 0) {
+            digit = num % 10;
+            num = num / 10;
+            result += digit;
+            System.out.print(digit + " + ");
+        }
+        System.out.println("\b\b= " + result);
+    }
+}
