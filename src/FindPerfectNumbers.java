e7383cf7e95455dd7720acd3bd3dca9c51f3837c
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-02-21 12:38:33 -0800

    Find Perfect Numbers Project

---
+++ src/FindPerfectNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FindPerfectNumbers {
+    public static void main(String[] args) {
+        int i, Number, Sum = 0 ;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n Please Enter any Number: ");
+        Number = scan.nextInt();
+
+        for(i = 1 ; i < Number ; i++) {
+            if(Number % i == 0)  {
+                Sum = Sum + i;
+            }
+        }
+        if (Sum == Number) {
+            System.out.format("\n% d is a Perfect Number", Number);
+        }
+        else {
+            System.out.format("\n% d is NOT a Perfect Number", Number);
+        }
+    }
+}
