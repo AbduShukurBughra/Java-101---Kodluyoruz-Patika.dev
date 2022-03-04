be533d7562fb8b07adb8bfafa2c5e0372fa4fbf7
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-03-03 21:02:38 -0800

    Prime Number Finder Program Project

---
+++ src/methood/PrimeNumberFinderProgram.java
@@ -0,0 +1,28 @@
+package methood;
+
+import java.util.Scanner;
+
+public class PrimeNumberFinderProgram {
+    static int CheckPrime(int i,int num)
+    {
+        if(num==i)
+            return 0;
+        else
+        if(num%i==0)
+            return 1;
+        else{
+            return CheckPrime(i+1,num);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner cs=new Scanner(System.in);
+        int num;
+        System.out.print("Enter your Number:");
+        num=cs.nextInt();
+        if(CheckPrime(2,num)==0)
+            System.out.print("It is a Prime Number.");
+        else
+            System.out.print("It is not a Prime Number.");
+        cs.close();
+    }
+}
