e1aa29d9091a9cc5583fb97ebe6b9c86bccf200c
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-03-07 18:43:31 -0800

    student Grading System Project

---
+++ src/classes/studentGradingSystem/Teacher.java
@@ -0,0 +1,14 @@
+package classes.studentGradingSystem;
+
+public class Teacher {
+    String name;
+    String phone;
+    String branch;
+
+
+    public Teacher(String name, String phone, String branch) {
+        this.name = name;
+        this.phone = phone;
+        this.branch = branch;
+    }
+}
